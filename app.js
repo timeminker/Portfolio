14ed1826018427ec2e24814b21e98abd901a276a
tim minker <timminker@gmail.com>
2022-04-28 10:10:58 -0400

    morming commit computer transfer

--- app.js
+++ app.js
@@ -58,6 +58,9 @@ let numOfImages = $('.carousel-images').children().length-1
 
 
 $(() => {
+  let currentImgIndex = 0
+  let numOfImages = $('.carousel-images').children().length-1
+
   $('#bio-pic').hover(bioPage)
   $('.extrude').on('click', changeFonts)
   $('.next').on('click', () => {
@@ -66,6 +69,16 @@ $(() => {
       currentImgIndex++
     } else {
       currentImgIndex = 0
+    }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+  $('.previous').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    if (currentImgIndex > 0) {
+      currentImgIndex--
+    } else {
+      currentImgIndex = numOfImages
     }
     $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
   })
